5ac06c124eb4e49338895206496edae56722337b
luiggi1611 <67654746+luiggi1611@users.noreply.github.com>
2023-11-03 12:07:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ SCOPES = [
 
 
 flow = InstalledAppFlow.from_client_secrets_file('client_secret.json',SCOPES)
-creds = flow.run_local_server(open_browser=False,port=0)
+#creds = flow.run_local_server(open_browser=False,port=0)
 #auth_url, _ = flow.authorization_url(prompt='consent')
 #creds = flow.run_console()
 
