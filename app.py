7d077a31d369be52864ce02eb5145add0af87b48
luiggi1611 <67654746+luiggi1611@users.noreply.github.com>
2023-11-03 12:26:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1173,7 +1173,7 @@ def admin_update_user(user_id):
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
 
 
 # app.run(debug=True)host='0.0.0.0', port=5000,v
